13ec428889d58b49efb732f841af242abed43950
jooaosanntos <101375975+jooaosanntos@users.noreply.github.com>
2022-07-16 11:46:26 -0300

    Refatorando Script
    Refazendo do zero o Script

--- Todo-list/script.js
+++ Todo-list/script.js
@@ -1,129 +1,151 @@
-const deletarTask = (task) => {
-    const array = JSON.parse(localStorage.getItem(0))
-
-    console.log(task)
-    array.splice(array.indexOf(task), 1)
-
-    localStorage.setItem(0, JSON.stringify(array))
-}
-
-const removerTask = (event) => {
-    const elemento = event.target.parentElement.parentElement
-    deletarTask(event.target.parentElement.previousSibling.innerHTML)
-
-    const containerTasks = document.querySelector(".container-tasks")
-    containerTasks.removeChild(elemento)
-}
-
-const concluirTask = (event) => {
-    const elemento = event.target
-    let elemento1
-    let elemento2
-    let ehCheckbox = false
-    if (elemento.classList.contains("task-completed")) {
-        elemento1 = elemento.parentElement.nextSibling
-        elemento2 = elemento
-        ehCheckbox = true
-    } else if (elemento.classList.contains("task-text")) {
-        elemento1 = elemento
-        elemento2 = elemento.previousSibling.children[0]
-    } else if (elemento.classList.contains("checkbox-wrapper")) {
-        elemento1 = elemento.nextSibling
-        elemento2 = elemento.children[0]
-    } else if (elemento.classList.contains("button-wrapper")) {
-        elemento1 = elemento.previousSibling
-        elemento2 = elemento.previousSibling.previousSibling.children[0]
-    } else return
-
-    if (!ehCheckbox) {
-        if (elemento2.checked) {
-            elemento2.checked = false
-            elemento1.style.textDecoration = "none"
-        } else {
-            elemento2.checked = true
-            elemento1.style.textDecoration = "line-through"
+/**
+ * Função que verifica se uma task já está salva no localStorage
+ * @param {*} task 
+ * @returns {boolean} true/false
+ */
+function taskJaSalva(task) {
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) == task.id){
+            return true
         }
-    } else {
-        if (elemento2.checked) elemento1.style.textDecoration = "line-through"
-        else elemento1.style.textDecoration = "none"
     }
-}
 
-const salvarTask = (task) => {
-    const array = JSON.parse(localStorage.getItem(0))
-    
-    array.push(task.children[1].innerHTML)
-    
-    localStorage.setItem(0, JSON.stringify(array))
-}
-
-const jaExiste = (texto) => {
-    const array  = JSON.parse(localStorage.getItem(0))
-    if (array.includes(texto)) return true
     return false
 }
 
-const adcionarTask = (texto, inicio=false) => {
-    if (!inicio && jaExiste(texto)) return
+/**
+ * Função que salva uma task no localStorage
+ */
+function salvarTaskLocalStora(task) {
+    if (!taskJaSalva(task)){
+        localStorage.setItem(task.id, JSON.stringify({
+            id: task.id,
+            conteudo: task.children[1].innerHTML,
+            concluida: task.getAttribute("checked-task")
+        }))
+    }
+}
+
+/**
+ * Função que gera uma id aleatório para uma task
+ */
+const gerarID = () => Math.floor(Date.now() * Math.random()).toString(36) +
+    Math.floor(Date.now() * Math.random()).toString(36)
+
+/**
+ * Função que recebe uma task e marca ela ou não a depender do valor de seu atributo checked-task
+ * @param {*} task 
+ */
+function concluirTask(task) {
+    const input = task.children[0].children[0]
+    const divTexto = task.children[1]
+
+    if (task.getAttribute("checked-task") == "true") {
+        input.checked = false
+        divTexto.style.textDecoration = "none"
+        task.setAttribute("checked-task", "false")
+
+        const objTask = JSON.parse(localStorage.getItem(task.id))
+        objTask["concluida"] = "false"
+        localStorage.setItem(task.id, JSON.stringify(objTask))
+    }
+    else {
+        input.checked = true
+        divTexto.style.textDecoration = "line-through"
+        task.setAttribute("checked-task", "true")
+
+        const objTask = JSON.parse(localStorage.getItem(task.id))
+        objTask["concluida"] = "true"
+        localStorage.setItem(task.id, JSON.stringify(objTask))
+    }
+}
+
+/**
+ * Função que remove uma task a partir do evento passado
+ * @param {*} event 
+ */
+function removerTask(event) {
+    const button = event.target
+    const task = button.parentElement.parentElement
+
+    const containerTasks = document.querySelector(".container-tasks")
+    containerTasks.removeChild(task)
+    localStorage.removeItem(task.id)
+}
+
+/**
+ * Função responsável por criar uma task a partir dos dados passados e adicionar ela à página
+ * @param {Object} dadosTask 
+ */
+function adicionarTask(dadosTask) {
     // Criar Task
     const task = document.createElement("div")
+    task.setAttribute("id", dadosTask.id)
     task.classList.add("task-element")
     task.classList.add("task")
-    
-    // Criar e adcionar checkbox a um wrapper
+    task.setAttribute("checked-task", "false")
+    document.querySelector(".container-tasks").appendChild(task)
+
+    // Criar e adicionar checkbox a um wrapper
     const checkboxWrapper = document.createElement("div")
     checkboxWrapper.classList.add("checkbox-wrapper")
-    
+
     const checkbox = document.createElement("input")
-    
+
     checkbox.classList.add("task-completed")
     checkbox.setAttribute("type", "checkbox")
     checkbox.setAttribute("name", "task-completed")
-    
+
     checkboxWrapper.appendChild(checkbox)
-    
+    task.appendChild(checkboxWrapper)
+
     // Criar div de texto
     const textoTask = document.createElement("div")
     textoTask.classList.add("task-text")
-    textoTask.innerHTML = texto
-    
-    // Criar e adcionar button a um wrapper
+    textoTask.innerHTML = dadosTask.conteudo
+    task.appendChild(textoTask)
+
+    // Criar e adicionar button a um wrapper
     const buttonWrapper = document.createElement("div")
     buttonWrapper.classList.add("button-wrapper")
-    
+
     const button = document.createElement("button")
     button.classList.add("delete-task")
     button.innerHTML = "X"
     button.addEventListener("click", removerTask)
-    
+
     buttonWrapper.appendChild(button)
-    
-    // Adcionar tudo à página
-    task.appendChild(checkboxWrapper)
-    task.appendChild(textoTask)
     task.appendChild(buttonWrapper)
-    
-    task.addEventListener("click", concluirTask)
-    document.querySelector(".container-tasks").appendChild(task)
 
-    if(!inicio) salvarTask(task)
+    if (dadosTask.concluida == "true") concluirTask(task)
+    task.addEventListener("click", function (event) {
+        concluirTask(this)
+    })
+
+    salvarTaskLocalStora(task)
 }
 
-if (localStorage.getItem(0) == null) {
-    const arrayTask = []
-    localStorage.setItem(0, JSON.stringify(arrayTask))
-}else{
-    const array = JSON.parse(localStorage.getItem(0))
-    array.forEach(texto => adcionarTask(texto, true))
-}
-
-const verificaDigitacao = (event) => {
-    if (event.key == "Enter") {
-        const valorInput = event.target.value
-        event.target.value = ""
-        if (valorInput.length > 0) adcionarTask(valorInput)
+/**
+ * Função responsável por pegar dados do usuário para criação de uma nova task. Os dados são obtidos a partir do input
+ */
+const inputTask = document.getElementById("add-task")
+inputTask.addEventListener("keyup", (event) => {
+    if (event.key.toLocaleLowerCase() == "enter") {
+        const input = event.target
+        adicionarTask({
+            id: gerarID(),
+            conteudo: input.value,
+            concluida: "false"
+        })
+        input.value = ""
     }
-}
+})
 
-const input = document.querySelector("#add-task")
-input.addEventListener("keyup", verificaDigitacao)
+/**
+ * Trecho de código que adiciona as tasks pré-existentes na página
+ */
+for(let i = 0; i < localStorage.length; i++){
+    const key = localStorage.key(i)
+    const dadosTask = JSON.parse(localStorage.getItem(key))
+    adicionarTask(dadosTask)
+}
